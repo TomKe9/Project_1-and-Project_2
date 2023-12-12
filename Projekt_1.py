2520d0b724314ced4ee0014d44f6e1105ee147d3
Tomáš Filipský <143267134+TomKe9@users.noreply.github.com>
2023-12-12 18:00:20 +0100

    Add files via upload

---
+++ Projekt_1.py
@@ -0,0 +1,125 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Tomáš Filipský
+email: tomas.filipsky@email.cz
+discord: tomas_53249
+"""
+
+import Projekt_1
+
+# zaregistrovaní uživatelé
+registrovani_uzivatele = {
+    "bob": "123",
+    "ann": "pass123",
+    "mike": "password123",
+    "liz": "pass123"
+}
+
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+
+# přihlášení uživatele
+username = input("username:")
+password = input("password:")
+
+# ověření uživatele
+if username in registrovani_uzivatele and registrovani_uzivatele[username] == password:
+    print(f"Welcome to the app, {username}")
+    print(f"We have {len(TEXTS)} texts to be analyzed.")
+else:
+    print("Unregistered user, terminating the program...")
+    exit()
+
+# Výběr textu k analýze
+selected_text = input("Enter a number btw. 1 and 3 to select: ")
+
+# Zkontrolovat platný výběr
+if not selected_text.isdigit() or int(selected_text) < 1 or int(selected_text) > len(TEXTS):
+    print("Invalid selection, terminating the program...")
+    exit()
+
+# Implementace analýzy textu
+selected_text = int(selected_text) - 1  # Převedení na index v seznamu
+text = TEXTS[selected_text]
+
+# Rozdělení textu na slova
+words = text.split()
+
+# Inicializace statistik
+word_count = 0
+titlecase_words = 0
+uppercase_words = 0
+lowercase_words = 0
+numeric_strings = 0
+numeric_sum = 0
+word_lengths = {}
+
+# Analýza slov
+for word in words:
+    # Počet slov
+    word_count += 1
+    
+    # Počet slov začínajících velkým písmenem
+    if word.istitle():
+        titlecase_words += 1
+        
+    # Počet slov psaných velkými písmeny
+    if word.isupper():
+        uppercase_words += 1
+        
+    # Počet slov psaných malými písmeny
+    if word.islower():
+        lowercase_words += 1
+    
+    # Počet čísel (ne cifer)
+    if word.isdigit():
+        numeric_strings += 1
+        numeric_sum += int(word)
+    
+    # Délka slova
+    word_length = len(word)
+    if word_length in word_lengths:
+        word_lengths[word_length] += 1
+    else:
+        word_lengths[word_length] = 1
+
+# Výstup statistik
+print(f"There are {word_count} words in the selected text.")
+print(f"There are {titlecase_words} titlecase words.")
+print(f"There are {uppercase_words} uppercase words.")
+print(f"There are {lowercase_words} lowercase words.")
+print(f"There are {numeric_strings} numeric strings.")
+print(f"The sum of all the numbers {numeric_sum}")
+
+# Vykreslení grafu
+print("\nLEN|  OCCURRENCES  |NR.")
+print("-" * 25)
+for length, occurrences in sorted(word_lengths.items()):
+    print(f"{length:3}|{'*' * occurrences:14}|{occurrences}")
+
---
+++ Projekt_2.py
@@ -0,0 +1,62 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: Tomáš Filipský
+email: tomas.filipsky@email.cz
+discord: tomas_53249
+"""
+
+import Projekt_2
+
+# Úvodní text
+import random 
+
+def pozdrav():
+
+    print("Hi there!")
+    print("-" * 25)
+    print("I've generated a random 4 digit number for you.", "Let's play a bulls and cows game.", sep="\n")
+    print("-" * 25)
+
+
+def generuj_tajne_cislo(lenght=4):
+    cislice = list(range(10))
+    random.shuffle(cislice)
+    return cislice[:4]
+
+def ohodnot_tah(tajne_cislo, tip):
+    bulls = 0
+    cows = 0
+
+    for i in range(len(tajne_cislo)):
+        if int(tip[i]) == tajne_cislo[i]:
+            bulls += 1
+        elif int(tip[i]) in tajne_cislo and int(tip[i]) != tajne_cislo[i]:
+            cows += 1
+
+    return bulls, cows
+
+def hra():
+    pozdrav()
+    tajne_cislo = generuj_tajne_cislo()
+    pokusy = 0
+
+    while True:
+        tip = input("Enter a number:\n>>> ")
+
+        if not tip.isdigit() or len(tip) != 4 or len(set(tip)) != 4 or tip[0] == '0':
+            print("Invalid input. Please enter 4 unique digits.")
+            continue
+
+        bulls, cows = ohodnot_tah(tajne_cislo, tip)
+
+        print(f"{bulls} {'bull' if bulls == 1 else 'bulls'}, {cows} {'cow' if cows == 1 else 'cows'}")
+
+        pokusy += 1
+
+        if bulls == 4:
+            print(f"Correct, you've guessed the right number in {pokusy} {'guess' if pokusy == 1 else 'guesses'}!")
+            break
+
+if __name__ == "__main__":
+    hra()
