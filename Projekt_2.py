a4205ffb941b8e6271dab154d2d24805d0bdbf61
Tomáš Filipský <143267134+TomKe9@users.noreply.github.com>
2024-02-25 14:06:09 +0100

    Projekt_2.py

--- Projekt_2.py
+++ Projekt_2.py
@@ -14,19 +14,19 @@ def pozdrav():
     print("I've generated a random 4 digit number for you.", "Let's play a bulls and cows game.", sep="\n")
     print("-" * 25)
 
-def generuj_tajne_cislo(lenght=4):
-    cislice = list(range(1, 10))  # Změna rozsahu na 1 až 9, aby se vyhnuli nule na začátku
+def generuj_tajne_cislo(length=4):
+    cislice = list(range(1, 10))
     random.shuffle(cislice)
-    return cislice[:4]
+    return ''.join(map(str, cislice[:4]))
 
 def ohodnot_tah(tajne_cislo, tip):
     bulls = 0
     cows = 0
 
-    for i in range(len(tajne_cislo)):
-        if int(tip[i]) == tajne_cislo[i]:
+    for i, cislice in enumerate(tajne_cislo):
+        if tip[i] == cislice:
             bulls += 1
-        elif int(tip[i]) in tajne_cislo and int(tip[i]) != tajne_cislo[i]:
+        elif tip[i] in tajne_cislo:
             cows += 1
 
     return bulls, cows
@@ -39,13 +39,25 @@ def hra():
     while True:
         tip = input("Enter a number:\n>>> ")
 
-        if not tip.isdigit() or len(tip) != 4 or len(set(tip)) != 4 or tip[0] == '0':
-            print("Invalid input. Please enter 4 unique digits.")
+        if not tip.isdigit():
+            print("Invalid input. Please enter a 4-digit number.")
+            continue
+        elif len(tip) != 4:
+            print("Invalid input. The number must have exactly 4 digits.")
+            continue
+        elif len(set(tip)) != 4:
+            print("Invalid input. Digits must be unique.")
+            continue
+        elif tip[0] == '0':
+            print("Invalid input. The number cannot start with zero.")
             continue
 
         bulls, cows = ohodnot_tah(tajne_cislo, tip)
 
-        print(f"{bulls} {'bull' if bulls == 1 else 'bulls'}, {cows} {'cow' if cows == 1 else 'cows'}")
+        if bulls == 0 and cows == 0:
+            print("No bulls or cows. None of the guessed digits are correct.")
+        else:
+            print(f"{bulls} {'bull' if bulls == 1 else 'bulls'}, {cows} {'cow' if cows == 1 else 'cows'}")
 
         pokusy += 1
 
